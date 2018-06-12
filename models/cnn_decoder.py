bd250e8c1fdfaeb9021bc2dee68c6f124cccb532
siddsax <saxenas2@login2.int.triton.aalto.fi>
2018-06-12 15:35:54 +0300

    working together training

--- models/cnn_decoder.py
+++ models/cnn_decoder.py
@@ -22,19 +22,29 @@ class cnn_decoder(nn.Module):
 
         self.params = params
         # Params Init
-        self.kernels = [Parameter(torch.Tensor(out_chan, in_chan, width).normal_(0, 0.05))
-                        for out_chan, in_chan, width in params.decoder_kernels]
-        self.biases = [Parameter(torch.Tensor(out_chan).normal_(0, 0.05))
-                       for out_chan, in_chan, width in params.decoder_kernels]
-
-        self._add_to_parameters(self.kernels, 'decoder_kernel')
-        self._add_to_parameters(self.biases, 'decoder_bias')
-
-
         self.out_size = self.params.decoder_kernels[-1][0]
-        self.fc = nn.Linear(self.out_size, self.params.vocab_size)
+        # self.kernels = [Parameter(torch.Tensor(out_chan, in_chan, width).normal_(0, 0.05))
+        #                 for out_chan, in_chan, width in params.decoder_kernels]
+        # self.biases = [Parameter(torch.Tensor(out_chan).normal_(0, 0.05))
+        #                for out_chan, in_chan, width in params.decoder_kernels]
 
-    def forward(self, decoder_input, z, drop_prob):
+        # self._add_to_parameters(self.kernels, 'decoder_kernel')
+        # self._add_to_parameters(self.biases, 'decoder_bias')
+
+
+        self.drp = nn.Dropout(p=params.drop_prob)
+        self.fc = nn.Linear(self.out_size, self.params.vocab_size)
+        self.conv_layers = []
+
+        for layer in range(len(params.decoder_kernels)):
+            [out_chan, in_chan, width] = params.decoder_kernels[layer]
+            layer = nn.Conv1d(in_chan, out_chan, width,
+                         dilation=self.params.decoder_dilations[layer],
+                         padding=self.params.decoder_paddings[layer])
+            self.conv_layers.append(layer)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid() 
+    def forward(self, decoder_input, z):
         """
         :param decoder_input: tensor with shape of [batch_size, seq_len, embed_size]
         :param z: sequence latent variable with shape of [batch_size, latent_variable_size]
@@ -52,29 +62,24 @@ class cnn_decoder(nn.Module):
 
         z = torch.cat([z] * seq_len, 1).view(batch_size, seq_len, self.params.Z_dim)
         decoder_input = torch.cat([decoder_input, z], 2)
-        decoder_input = nn.Dropout(decoder_input, drop_prob)
+        decoder_input = self.drp(decoder_input)
 
         # x is tensor with shape [batch_size, input_size=in_channels, seq_len=input_width]
         x = decoder_input.transpose(1, 2).contiguous()
 
-        for layer, kernel in enumerate(self.kernels):
+        for layer in range(len(self.params.decoder_kernels)):
             # apply conv layer with non-linearity and drop last elements of sequence to perfrom input shifting
-            x = nn.conv1d(x, kernel,
-                         bias=self.biases[layer],
-                         dilation=self.params.decoder_dilations[layer],
-                         padding=self.params.decoder_paddings[layer])
-
+            x = self.conv_layers[layer](x)
             x_width = x.size()[2]
             x = x[:, :, :(x_width - self.params.decoder_paddings[layer])].contiguous()
-
-            x = nn.relu(x)
+            x = self.relu(x)
 
 
         x = x.transpose(1, 2).contiguous()
         x = x.view(-1, self.out_size)
         x = self.fc(x)
         result = x.view(-1, seq_len, self.params.vocab_size)
-
+        result = self.sigmoid(result)
         return result
 
     def _add_to_parameters(self, parameters, name):
