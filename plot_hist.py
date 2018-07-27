d3d6c9d90977074948193aa8cb544182bcc37b47
Siddhartha Saxena <ss.siddharthasaxena@gmail.com>
2018-07-27 20:04:00 +0300

    .

--- plot_hist.py
+++ plot_hist.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-deep')
 from sklearn import preprocessing
 
+
 p = np.load('regen_data.npy')
 g = np.load('../datasets/Eurlex/eurlex_docs/x_te.npy')
 #g = np.load('../datasets/Eurlex/eurlex_docs/x_20.npy')
@@ -15,9 +16,10 @@ g = scaler.transform(g)
 
 
 
-ax1 = np.nonzero(l1)
-ax2 = np.nonzero(l2)
+ax1 = np.nonzero(p[0])
+ax2 = np.nonzero(g[0])
 ax = np.intersect1d(ax1, ax2)
+
 ind = np.arange(ax.shape[-1])
 fig, x = plt.subplots()
 w = .1
@@ -26,11 +28,11 @@ l1 = p[0, ax]
 l2 = g[0, ax]
 
 p1 = x.bar(ind, l1, w, color='r', bottom=0)
-p1 = x.bar(ind+w, l2, w, color='y', bottom=0)
+p2 = x.bar(ind+w, l2, w, color='y', bottom=0)
 
 
 
-x.set_xticks(ind + width / 2)
+x.set_xticks(ind)
 x.autoscale_view()
 plt.show()
 
